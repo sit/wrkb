a2a1930c899651782284aabbd9172feee0136e9a
Emil Sit <sit@emilsit.net>
2025-03-09 17:19:49 -0400

    fix(lint): ruff warnings

--- wrkb.py
+++ wrkb.py
@@ -1,8 +1,9 @@
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(description="wrkb - a command line tool")
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1.0")
 
     args = parser.parse_args()
 
